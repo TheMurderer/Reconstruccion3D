d2df75ad007822ce259428a7c4bef39a7badbe69
José Manuel Serrano Mármol <serrano5510@hotmail.com>
2013-02-26 12:05:02 +0100

    Matching más o menos conseguido. Harris,SIFT,SURF

--- Test/main.cpp
+++ Test/main.cpp
@@ -9,15 +9,53 @@ using namespace std;
 #define _SURF_ 1
 //#define _SIFT_ 1
 
+//Variables globales
+int thresh = 150;
+int max_thresh = 255;
+
+//Harris - detector de esquinas
+void harris(Mat src,Mat &src_gray, vector<KeyPoint>& keypoints){
+    Mat dst, dst_norm, dst_norm_scaled;
+    dst = Mat::zeros( src.size(), CV_32FC1 );
+
+    /// Detector parameters
+    int blockSize = 2;
+    int apertureSize = 3;
+    double k = 0.04;
+
+    /// Detecting corners
+    cornerHarris( src_gray, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
+
+    /// Normalizing
+    normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
+    convertScaleAbs( dst_norm, dst_norm_scaled );
+
+    /// Drawing a circle around corners
+    for( int j = 0; j < dst_norm.rows ; j++ )
+       { for( int i = 0; i < dst_norm.cols; i++ )
+            {
+              if( (int) dst_norm.at<float>(j,i) > thresh )
+                {
+                 circle( dst_norm_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0 );
+                 Point p = Point( i, j );
+                 keypoints.push_back(KeyPoint(p.x,p.y,5));
+                }
+            }
+       }
+    /// Showing the result
+    //namedWindow( corners_window, CV_WINDOW_AUTOSIZE );
+    //imshow("Harris",dst_norm_scaled );
+}
+
 // Código de ayuda para el uso de los detectores SURF o SIFT
 void surf(Mat& img1, vector<KeyPoint>& keypoints){
         #if  _SURF_
-        int minHessian = 5000; // valor de unbral para la deteccion de puntos, cambiar si es necesario
+        int minHessian = 1000; // valor de unbral para la deteccion de puntos, cambiar si es necesario
         SurfFeatureDetector detector( minHessian );
         detector.detect(img1, keypoints);
         #else if _SIFT_
         double sigma=1;
-        int puntos=1000;
+        int puntos=10000;
         SiftFeatureDetector detector(puntos,3,0.06,10,sigma);
         detector.detect(img1, keypoints);
         #endif
@@ -34,12 +72,39 @@ void computeMatching(Mat& img1, Mat& img2,vector<KeyPoint>& keypoints1,vector<Ke
         extractor.compute(img1, keypoints1, descriptors1);
         extractor.compute(img2, keypoints2, descriptors2);
 
-        // matching descriptors
-        bool crossCheck=1;
+        FlannBasedMatcher matcher;
+        matcher.match(descriptors1,descriptors2,matches);
 
-        //BFMatcher match;
-        BFMatcher matcher(NORM_L2,crossCheck);
-        matcher.match(descriptors1, descriptors2, matches);
+        double max_dist = 0; double min_dist = 100;
+
+        //-- Quick calculation of max and min distances between keypoints
+        for( int i = 0; i < descriptors1.rows; i++ ){
+           double dist = matches[i].distance;
+           if( dist < min_dist ) min_dist = dist;
+           if( dist > max_dist ) max_dist = dist;
+         }
+
+         printf("-- Max dist : %f \n", max_dist );
+         printf("-- Min dist : %f \n", min_dist );
+
+         //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist )
+         //-- PS.- radiusMatch can also be used here.
+         std::vector< DMatch > good_matches;
+
+         for( int i = 0; i < descriptors1.rows; i++ ){
+             if( matches[i].distance < 2*min_dist ){
+                 good_matches.push_back( matches[i]);
+             }
+         }
+
+         //-- Draw only "good" matches
+         Mat img_matches;
+         drawMatches( img1, keypoints1, img2, keypoints2,
+                        good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                        vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+         //-- Show detected matches
+         imshow( "Good Matches", img_matches );
 }
 
 void imprimirKeipoints(vector<KeyPoint> puntos){
@@ -57,20 +122,21 @@ int main(int argc, char* argv[]) {
             return -1;
         }*/
 
-        Mat img1 = imread("Imagenes/img1.bmp",CV_LOAD_IMAGE_GRAYSCALE);
-        Mat img2 = imread("Imagenes/img2.bmp",CV_LOAD_IMAGE_GRAYSCALE);
+        Mat img1,img2,img1_gray,img2_gray;
+        img1 = imread("Imagenes/Dino/viff.002.ppm");
+        img2 = imread("Imagenes/Dino/viff.003.ppm");
+        cvtColor( img1, img1_gray, CV_BGR2GRAY );
+        cvtColor( img2, img2_gray, CV_BGR2GRAY );
 
-        //Aplicacmos Canny  -- Detección de bordes
-        Mat img1Canny,img2Canny;
-        Canny(img1,img1Canny,50,200,3);
-        Canny(img1,img2Canny,50,200,3);
 
         // Obtenemos los puntos en correspondencias de forma automática usando SURF
         vector<KeyPoint> keypoints1, keypoints2;
         vector<DMatch> matches;
 
-        surf(img1Canny, keypoints1);
-        surf(img2Canny, keypoints2);
+        surf(img1, keypoints1);
+        surf(img2, keypoints2);
+        //harris(img1,img1_gray,keypoints1);
+        //harris(img2,img2_gray,keypoints2);
 
         //Señalamos los puntos encontrados sobre la imangen
 
@@ -79,13 +145,7 @@ int main(int argc, char* argv[]) {
         //drawKeypoints(img2,keypoints2,salidaImg2);
 
         //Hacemos el matching de los puntos encontrados en las imagenes
-        computeMatching(img1Canny, img2Canny, keypoints1, keypoints2, matches);
-
-        Mat matchImg;
-        drawMatches(img1Canny,keypoints1,img2Canny,keypoints2,matches,matchImg);
-
-
-        imshow("Match img1 | img2",matchImg);
+        computeMatching(img1, img2, keypoints1, keypoints2, matches);
 
 
         //vector <Point2f> puntosImagen1, puntosImagen2;
